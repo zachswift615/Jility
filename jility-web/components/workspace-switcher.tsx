046d8426d6e27dc2b80f054cfb5e86889019335e
zachswift <zacharyswift@gmail.com>
2025-11-06 10:50:34 -0600

    feat: add workspace switcher component

---
+++ jility-web/components/workspace-switcher.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import { useWorkspace } from '@/lib/workspace-context'
+import { Check, ChevronsUpDown, Plus } from 'lucide-react'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Button } from '@/components/ui/button'
+
+export function WorkspaceSwitcher() {
+  const { currentWorkspace, workspaces, switchWorkspace } = useWorkspace()
+
+  if (!currentWorkspace) {
+    return null
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          className="w-[200px] justify-between"
+        >
+          <span className="truncate">{currentWorkspace.name}</span>
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-[200px]" align="start">
+        <DropdownMenuLabel>Workspaces</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        {workspaces.map((workspace) => (
+          <DropdownMenuItem
+            key={workspace.id}
+            onClick={() => switchWorkspace(workspace.slug)}
+            className="cursor-pointer"
+          >
+            <Check
+              className={`mr-2 h-4 w-4 ${
+                workspace.id === currentWorkspace.id
+                  ? 'opacity-100'
+                  : 'opacity-0'
+              }`}
+            />
+            <span className="truncate">{workspace.name}</span>
+          </DropdownMenuItem>
+        ))}
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className="cursor-pointer">
+          <Plus className="mr-2 h-4 w-4" />
+          Create workspace
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
